b2e0b76d1baf9408c8fc1b36a5fb37fca929dcab
houkensjtu <houkensjtu@gmail.com>
2022-03-14 12:52:14 +0800

    Updated speed test.

--- debug/ti_speed_test.py
+++ debug/ti_speed_test.py
@@ -1,7 +1,7 @@
 import taichi as ti
 import time
 
-ti.init(arch=ti.cpu)
+ti.init(arch=ti.gpu, kernel_profiler=True)
 
 n = 1024*1024
 v1 = ti.field(dtype=ti.f64, shape = n)
@@ -16,6 +16,7 @@ def init():
 @ti.kernel
 def reduce_para(v1:ti.template(), v2:ti.template())->ti.f64:
     n = v1.shape[0]
+    # sum = ti.cast(0.0, ti.f64)
     sum = 0.0
     for i in range(n):
         sum += v1[i]*v2[i]
@@ -24,7 +25,8 @@ def reduce_para(v1:ti.template(), v2:ti.template())->ti.f64:
 @ti.kernel
 def reduce_seri(v1:ti.template(), v2:ti.template())->ti.f64:
     n = v1.shape[0]
-    sum = 0.0
+    # sum = ti.cast(0.0, ti.f64)
+    sum = 0.0    
     for _ in range(1):
         for i in range(n):
             sum += v1[i]*v2[i]
@@ -39,17 +41,22 @@ def reduce(v1, v2, n):
 print('Initializing...')
 init()
 
-print('Reducing in Python scope...')
-start = time.time()
-print(reduce(v1, v2, n))
-print(time.time() - start)
+# print('Reducing in Python scope...')
+# start = time.time()
+# print(reduce(v1, v2, n))
+# print(time.time() - start)
 
 print('Reducing in Taichi scope parallel...')
 start = time.time()
 print(reduce_para(v1, v2))
 print(time.time() - start)
+ti.profiler.print_kernel_profiler_info('trace')
+ti.profiler.clear_kernel_profiler_info()
+
 
 print('Reducing in Taichi scope serial...')
 start = time.time()
 print(reduce_seri(v1, v2))
 print(time.time() - start)
+ti.profiler.print_kernel_profiler_info('trace')
+ti.profiler.clear_kernel_profiler_info()
