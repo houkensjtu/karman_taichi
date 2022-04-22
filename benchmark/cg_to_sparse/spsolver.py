81515fcf1ed15607432961613938eaeb9d6d9170
houkensjtu <houkensjtu@gmail.com>
2022-04-22 20:38:26 +0800

    Refactored sparse solver into a separate class.

---
+++ benchmark/cg_to_sparse/spsolver.py
@@ -0,0 +1,52 @@
+import taichi as ti
+import numpy as np
+
+ti.init(arch=ti.cpu)
+
+class SparsePoissonSolver:
+    def __init__(self, n=128, solver_type='LU'):
+        self.N = n
+        self.solver_type = solver_type
+        
+    # Build lfs of the equation using SparseMatrixBuilder
+    def build_ASparse(self):
+        n = self.N * self.N
+        k = ti.linalg.SparseMatrixBuilder(n,n,max_num_triplets=5*n) # Create the builder
+        @ti.kernel
+        def fill(A: ti.types.sparse_matrix_builder()): # Fill the builder with data
+            for i in range(n):
+                A[i, i] += 4.0
+                if i-1 >= 0 and i%self.N !=0:
+                    A[i, i-1] -= 1.0
+                if i-self.N >= 0:
+                    A[i, i-self.N] -= 1.0
+                if i+1 < n and i%self.N!=self.N-1:
+                    A[i, i+1] -= 1.0
+                if i+self.N < n:
+                    A[i, i+self.N] -= 1.0
+        fill(k)
+        A = k.build() # Build the matrix using the builder
+        return A
+
+    def build_b(self):
+        b = np.zeros(shape=(self.N,self.N))
+        for i,j in np.ndindex(self.N, self.N):
+            xl = i / self.N
+            yl = j / self.N
+            b[i, j] = ti.sin(2.0 * np.pi * xl) * ti.sin(2.0 * np.pi * yl)
+        b = b.flatten(order='C')
+        np.savetxt('b.csv', b, delimiter=',')
+        return b.flatten(order='C')
+
+    def solve(self):
+        asp = self.build_ASparse()
+        solver = ti.linalg.SparseSolver(solver_type=self.solver_type)
+        solver.analyze_pattern(asp)
+        solver.factorize(asp)
+        b = self.build_b()
+        return solver.solve(b)
+
+
+sp = SparsePoissonSolver()
+sp.solve()
+
--- benchmark/cg_to_sparse/test_cg_sparse.py
+++ benchmark/cg_to_sparse/test_cg_sparse.py
@@ -1,4 +1,5 @@
 from cgsolver import CGSolver
+from spsolver import SparsePoissonSolver
 import taichi as ti
 import numpy as np
 import time
@@ -7,34 +8,28 @@ ti.init(arch=ti.x64, kernel_profiler=True)
 
 # Solve a 128x128 2-D Poisson Equation
 # There are 128^2 unknowns in total
-
+psize = 128
 # Solve in Taichi using custom CG
 # A is implicitly represented in compute_Ap()
 now = time.time()
 print('Solving using CGSolver...')
-solver = CGSolver(128, 1e-16, quiet=False) # quiet=False to print residual
+solver = CGSolver(psize, 1e-16, quiet=False) # quiet=False to print residual
 solver.solve()
 ti.profiler.print_kernel_profiler_info()
 print('>>> Time spent using CG:', time.time() - now, 'sec')
 
 # Solve in Taichi using SparseMatrixSolver
 now = time.time()
-print('Building sparse matrix...')
-asp = solver.build_ASparse()
-b = solver.build_b()
-print('Solving using SparseSolver...')
-spsolver = ti.linalg.SparseSolver(solver_type='LU')
-spsolver.analyze_pattern(asp)
-spsolver.factorize(asp)
-xsp = spsolver.solve(b)
-isSuccess = spsolver.info()
-print('Sparse solver succeded?', isSuccess)
+# print('Solving using SparseSolver...')
+spsolver = SparsePoissonSolver(psize, 'LU')
+xsp = spsolver.solve()
 print('>>> Time spent using SparseMatrixSolver:', time.time() - now, 'sec')
 
 # Compare the results
 print('Fetching results...') # Building matrix in Numpy is very slow...
 xti = solver.build_x()       # Fetch solution from CG solver
 a = solver.build_A()         # Build the lfs of the equation
+b = solver.build_b()         # Build the rfs of the equation
 print('Comparing the results...')
 print('Residual cg:', np.linalg.norm(b - np.dot(a, xti)))
 print('Residual sp:', np.linalg.norm(b - np.dot(a, xsp)))
