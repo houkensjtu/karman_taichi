02e2dff255f5b247dbc8359e3ab89d959c886092
houkensjtu <houkensjtu@gmail.com>
2022-05-03 11:52:10 +0800

    Add display class to facilitate field displaying.

---
+++ display.py
@@ -0,0 +1,43 @@
+import taichi as ti
+
+@ti.data_oriented
+class Display:
+    def __init__(self, SIMPLESolver, *args):
+        self.solver = SIMPLESolver
+        self.nx = self.solver.nx
+        self.ny = self.solver.ny
+        self.real = self.solver.real
+        self.udisp = ti.field(dtype=self.solver.real, shape=((self.nx+2), (self.ny+2)))
+        self.vdisp = ti.field(dtype=self.solver.real, shape=((self.nx+2), (self.ny+2)))
+        self.pdisp = ti.field(dtype=self.solver.real, shape=((self.nx+2), (self.ny+2)))                
+
+    @ti.func
+    def scale_field(self, f):
+        f_max = -1.0e9
+        f_min = 1.0e9
+        for i,j in f:
+            if f[i,j] > f_max:
+                f_max = f[i,j]
+            if f[i,j] < f_min:
+                f_min = f[i,j]
+        for i,j in f:
+            f[i,j] = (f[i,j] - f_min) / (f_max - f_min + 1.0e-9)
+        
+    @ti.kernel
+    def post_process_field(self):
+        for i,j in ti.ndrange(self.nx+2, self.ny+2):
+            self.udisp[i,j] = 0.5 * (self.solver.u[i,j] + self.solver.u[i+1,j])
+            self.vdisp[i,j] = 0.5 * (self.solver.v[i,j] + self.solver.v[i,j+1])
+            self.pdisp[i,j] = self.solver.p[i,j]
+        self.scale_field(self.udisp)
+        self.scale_field(self.vdisp)
+        self.scale_field(self.pdisp)                
+            
+    def display(self, filename):
+        import numpy as np        
+        self.post_process_field()
+        gui = ti.GUI("SIMPLESolver", ((self.nx+2),3*(self.ny+2)), show_gui=False)
+        img = np.concatenate((self.udisp.to_numpy(), self.vdisp.to_numpy(), self.pdisp.to_numpy()), axis=1)
+        gui.set_image(img)
+        gui.show(filename)
+
--- simple.py
+++ simple.py
@@ -1,4 +1,5 @@
 import taichi as ti
+from display import Display
 
 ti.init(arch=ti.cpu, default_fp=ti.f64)
 
@@ -12,10 +13,21 @@ class SIMPLESolver:
         self.v  = ti.field(dtype=self.real, shape=(nx+2, ny+3))
         self.p  = ti.field(dtype=self.real, shape=(nx+2, ny+2))
         self.ct = ti.field(dtype=self.real, shape=(nx+2, ny+2))   # Cell type
-        self.disp = ti.field(dtype=self.real, shape=(3*(nx+2), 3*(ny+2)))
+        self.disp = Display(self)
 
-    def display(self):
-        print('Displaying the flow field...')
+    @ti.kernel
+    def init(self):
+        for i,j in self.u:
+            self.u[i,j] = - i
+        for i,j in self.v:
+            self.v[i,j] = - j 
+        for i,j in self.p:
+            self.p[i,j] = - i * j
 
-ssolver = SIMPLESolver(64, 320)
-ssolver.display()
+    def solve(self):
+        self.init()
+        for step in range(3):
+            self.disp.display(f'{step}.png')
+
+ssolver = SIMPLESolver(128, 128)
+ssolver.solve()
