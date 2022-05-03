28ffb90d353c6f0c6253bada5dbac8677d91d81a
houkensjtu <houkensjtu@gmail.com>
2022-05-03 13:06:42 +0800

    Add file saving utilities.

--- simple.py
+++ simple.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import numpy as np
 from display import Display
 
 ti.init(arch=ti.cpu, default_fp=ti.f64)
@@ -15,6 +16,11 @@ class SIMPLESolver:
         self.ct = ti.field(dtype=self.real, shape=(nx+2, ny+2))   # Cell type
         self.disp = Display(self)
 
+
+    def dump_matrix(self, step):
+        for k,v in {'u':self.u, 'v':self.v, 'p':self.p}.items():
+            np.savetxt(f'log/{k}-{step:06}.csv', v.to_numpy(), delimiter=',')
+
     @ti.kernel
     def init(self):
         for i,j in self.u:
@@ -27,7 +33,8 @@ class SIMPLESolver:
     def solve(self):
         self.init()
         for step in range(3):
-            self.disp.display(f'{step}.png')
+            self.disp.display(f'log/{step:06}.png')
+            self.dump_matrix(step)
 
 ssolver = SIMPLESolver(128, 128)
 ssolver.solve()
